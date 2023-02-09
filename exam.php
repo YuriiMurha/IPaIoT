4ce4732f8463128fd40cdc25a884cc87488e8bdf
YuriiMurha <114644393+YuriiMurha@users.noreply.github.com>
2023-02-09 09:34:22 +0100

    Update exam.php

--- exam.php
+++ exam.php
@@ -3,4 +3,9 @@
   echo "button " . $button;
 ?>
 <input name="led" type="checkbox" value="1" 
-<?php if ($row['led'] == 1) { echo "checked='checked'"; } ?>>
+<?php if(!empty($_POST['led'])){
+ echo "LED ON"
+} else {
+ echo "LED OFF"
+} ?>>
+
