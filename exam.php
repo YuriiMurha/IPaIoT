530342beff56cd5754ebaa876c18c3deca62a600
YuriiMurha <114644393+YuriiMurha@users.noreply.github.com>
2023-02-09 09:39:11 +0100

    Update exam.php

--- exam.php
+++ exam.php
@@ -1,9 +1,4 @@
-<input name="led" type="checkbox" value="1"
 <?php
-if(!empty($_POST['led'])){
- echo "LED ON"
-} else {
- echo "LED OFF"
-}
 $button = $_GET["button"];
-echo "button " . $button; ?>>
+echo "button " . $button; 
+?>
