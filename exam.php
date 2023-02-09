750b57682d5fe02628a090bab3029a89d7cbca58
YuriiMurha <114644393+YuriiMurha@users.noreply.github.com>
2023-02-09 08:45:53 +0100

    Update exam.php

--- exam.php
+++ exam.php
@@ -2,5 +2,5 @@
   //<input type="checkbox" name="led" id="led">;
   //<label for="led">LED</label>;
   $button = $_GET["button"];
-  echo "<h1>button is <\h1>" . $button;
+  echo "<h1>button is </h1>" . $button;
   ?>
