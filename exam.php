6375bf65e97293680c4198073482202ed85f3b1a
YuriiMurha <114644393+YuriiMurha@users.noreply.github.com>
2023-02-09 09:30:08 +0100

    Update exam.php

--- exam.php
+++ exam.php
@@ -2,12 +2,5 @@
   $button = $_GET["button"];
   echo "button " . $button;
 ?>
-<input type="checkbox" name="led" id="led">
-<label for="led">LED</label>
-if (isset($_GET['check1'])) {
-  LED ON
-} 
-else
-{
-  LED OFF
-}
+<input name="led" type="checkbox" value="1" 
+<?php if ($row['led'] == 1) { echo "checked='checked'"; } ?>>
