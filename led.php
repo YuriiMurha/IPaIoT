33593cce578aa68579c304ea2d553d7640625515
YuriiMurha <114644393+YuriiMurha@users.noreply.github.com>
2023-02-09 09:58:57 +0100

    Update led.php

--- led.php
+++ led.php
@@ -1,3 +1,12 @@
 <input type="checkbox" name="led" value="led_value">
 <label for="led">LED</label>
-echo $_POST['led']; // 'checkbox_value'
+
+<?php
+
+$ison = filter_input(INPUT_POST, 'led', FILTER_SANITIZE_STRING);
+if ($ison) {
+	echo 'LED is ON';
+} else {
+	echo 'LED is off';
+}
+?>
