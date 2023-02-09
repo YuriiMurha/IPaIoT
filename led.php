212183572ac703cbd542181e1b94783ae5f5cccb
YuriiMurha <114644393+YuriiMurha@users.noreply.github.com>
2023-02-09 09:42:01 +0100

    Update led.php

--- led.php
+++ led.php
@@ -1,7 +1,6 @@
-<input name="led" type="checkbox" value="1"
-<?php 
-if(!empty($_POST['led'])){
- echo "LED ON"
-} else {
- echo "LED OFF"
-}?>>
+<input name="led" type="checkbox" value="1"> 
+//if(!empty($_POST['led'])){
+// echo "LED ON"
+//} else {
+// echo "LED OFF"
+
